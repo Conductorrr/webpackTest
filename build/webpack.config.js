2ccd0a2e1aca75bcbd28ad313b918409611aeb3f
zhulili <403605792@qq.com>
2021-07-25 17:31:01 +0800

    css相关配置

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,29 +1,76 @@
 // webpack.config.js
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const {
+  CleanWebpackPlugin
+} = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-    mode:'development', // 通过配置mode来告诉webpack当前运行的环境   开发模式
-    entry: {
-      main: path.resolve(__dirname,'../src/main.js'),    // 入口文件
-      header: path.resolve(__dirname, '../src/header.js')
+  mode: 'development', // 通过配置mode来告诉webpack当前运行的环境   开发模式
+  entry: { // 入口文件
+    main: path.resolve(__dirname, '../src/main.js'),
+    header: path.resolve(__dirname, '../src/header.js')
+  },
+  output: {
+    filename: '[name].[hash:8].js', // 打包后的文件名称
+    path: path.resolve(__dirname, '../dist') // 打包后的目录
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../public/index.html'), // 模板文件地址
+      filename: 'index.html',
+      chunks: ['main']
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../public/index.html'), // 模板文件地址
+      filename: 'header.html',
+      chunks: ['header']
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].[hash].css',
+      chunkFilename: '[id].css'
+    }),
+  ],
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
     },
-    output: {
-      filename: '[name].[hash:8].js',      // 打包后的文件名称
-      path: path.resolve(__dirname,'../dist')  // 打包后的目录
+    {
+      test: /\.less$/,
+      use: [
+        'style-loader', 
+        MiniCssExtractPlugin.loader,
+        'css-loader', 
+        {
+          loader: 'postcss-loader',
+          options:{
+            postcssOptions: {
+              plugins:[require('autoprefixer')] //autofixer是postcss的功能插件。它可以自动在样式中添加浏览器厂商前缀，避免手动处理样式兼容问题
+            }
+          },
+        },
+        'less-loader',
+      ]
     },
-    plugins:[
-      new HtmlWebpackPlugin({
-        template:path.resolve(__dirname,'../public/index.html'), // 模板文件地址
-        filename: 'index.html',
-        chunks: ['main']
-      }),
-      new HtmlWebpackPlugin({
-        template:path.resolve(__dirname,'../public/index.html'), // 模板文件地址
-        filename: 'header.html',
-        chunks: ['header']
-      }),
-      new CleanWebpackPlugin()
     ]
+  }
 }
+
+/*
+// 在 Loader 需要传入很多参数时，你还可以通过一个 Object 来描述
+use: [
+  {
+    loader:'babel-loader',
+    options:{
+      cacheDirectory:true,
+    },
+    // enforce:'post' 的含义是把该 Loader 的执行顺序放到最后
+    // enforce 的值还可以是 pre，代表把 Loader 的执行顺序放到最前面
+    enforce:'post'
+  },
+  // 省略其它 Loader
+]
+*/
